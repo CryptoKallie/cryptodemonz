569c1e1a25ea0c393b7b36bc6619e915f9708ae8
kanedaaaa <samflynn19121@gmail.com>
2021-09-01 02:43:10 +0200

    primitive fix for gas limit

--- src/modules/MintForm.js
+++ src/modules/MintForm.js
@@ -12,32 +12,29 @@ const MintForm = ({ account, active }) => {
     const [ethPrice, setEthPrice] = useState(1);
     const [amount, setAmount] = useState(1);
     const [popupData, setPopupData] = useState([false]);
-    const [gasPrice, setGasPrice] = useState('');
-
+    //const [gasPrice, setGasPrice] = useState('');
 
     const buyToken = async () => {
-            getGasPrice();
+        getGasPrice();
         if (active) {
             try {
-                
-                
-                setPopupData(['gas'])
+                setPopupData(["gas"]);
 
                 await contract.methods.mintToken(amount).send({
                     from: account,
                     value: amount * web3.utils.toWei("0.06", "ether"),
-                    //gasLimit: "285000",
-                    gasLimit: gasPrice,
+                    gasLimit: "285000",
+                    //gasLimit: gasPrice,
                     maxPriorityFeePerGas: null,
                     maxFeePerGas: null,
                 });
             } catch (err) {
                 console.log(err);
                 //TODO dont throw error alert if user just rejects transaction
-                setPopupData(['error', err]);
+                setPopupData(["error", err]);
             }
         } else {
-            setPopupData(['metamask'])
+            setPopupData(["metamask"]);
         }
     };
     //this has a visual bug, user can put as many zeros as he wants before first non-zero int
@@ -62,142 +59,163 @@ const MintForm = ({ account, active }) => {
     // }
 
     const getGasPrice = () => {
-        web3.eth.getGasPrice()
-        .then(price => {
-            let gwei = web3.utils.fromWei(price, 'gwei')
+        web3.eth.getGasPrice().then((price) => {
+            let gwei = web3.utils.fromWei(price, "gwei");
             console.log(gwei);
         });
-    }
+    };
 
     const hidePopup = () => {
         setPopupData([false]);
-    }
+    };
 
     const errorController = () => {
         if (popupData[0] === false) {
             return;
-        } else
-        if (popupData[0] === 'error') {
+        } else if (popupData[0] === "error") {
             return (
                 <div className="modal-container">
                     <div className="alert alert-dismissible alert-danger">
-                        <button type="button" className="btn-close" onClick={hidePopup}></button>
-                        <strong>Oh snap!</strong> 
-                        <p className="text-dark">It looks like an Error occured, please copy the text below and send a PM to the CDZ Report bot</p>
-                        <textarea className="form-control" id="exampleTextarea" rows="3">
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={hidePopup}
+                        ></button>
+                        <strong>Oh snap!</strong>
+                        <p className="text-dark">
+                            It looks like an Error occured, please copy the text
+                            below and send a PM to the CDZ Report bot
+                        </p>
+                        <textarea
+                            className="form-control"
+                            id="exampleTextarea"
+                            rows="3"
+                        >
                             {popupData[1]}
                         </textarea>
                     </div>
                 </div>
             );
-        } else if (popupData[0] === 'gas') {
+        } else if (popupData[0] === "gas") {
             return (
                 <div className="modal-container">
                     <div className="alert alert-dismissible alert-warning">
-                        <button type="button" className="btn-close" onClick={hidePopup}></button>
-                        <strong>Warning!</strong> 
-                            <p className="text-dark">Please MAKE SURE that suggested fees are
-                            normal, if not edit!. If you don't know
-                            how, please open ticket on our discord
-                            server. We are trying to resolve this
-                            problem.</p>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={hidePopup}
+                        ></button>
+                        <strong>Warning!</strong>
+                        <p className="text-dark">
+                            Gas fee problem is partially resolved, it will give
+                            you reasonable price which is under 200$. But
+                            consider that it's statically adjuted (current
+                            default is 285000 limit). We are working to develop
+                            gas management strategy and offer the best prices,
+                            this will be available in next patch (v0.0.3)
+                        </p>
                     </div>
                 </div>
-            )
-        } else if (popupData[0] === 'metamask') {
+            );
+        } else if (popupData[0] === "metamask") {
             return (
                 <div className="modal-container">
-                        <div className="alert alert-dismissible alert-warning">
-                            <button type="button" className="btn-close" onClick={hidePopup}></button>
-                            <strong>Warning!</strong> 
-                                <p className="text-dark">Please connect metamask with the button in the top right corner</p>
+                    <div className="alert alert-dismissible alert-warning">
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={hidePopup}
+                        ></button>
+                        <strong>Warning!</strong>
+                        <p className="text-dark">
+                            Please connect metamask with the button in the top
+                            right corner
+                        </p>
                     </div>
                 </div>
-            )
+            );
         } else {
-            return <p>THIS IS A TEST</p>
+            return <p>THIS IS A TEST</p>;
         }
-    }
+    };
 
     return (
         <div>
-           {errorController()}
+            {errorController()}
+            <div className="row">
+                <div className="col-lg-12 form-tabbed">
+                    <div className="form-group text-center">
+                        <label htmlFor="exampleInputEmail1">
+                            <img
+                                src="images/welcome.gif"
+                                className="welcome-gif"
+                                alt="Welcome to the minting section"
+                            />
 
-                <div className="row">
-                    <div className="col-lg-12 form-tabbed">
-                        <div className="form-group text-center">
-                            <label htmlFor="exampleInputEmail1">
-
-                                <img
-                                    src="images/welcome.gif"
-                                    className="welcome-gif"
-                                    alt="Welcome to the minting section"
-                                />
-
-                                <h1>
-                                    <u>Minting</u>
-                                </h1>
-                            </label>
-                            <div className="row">
-                                <div className="col-md-5 text-center">
-                                    <div className="input-group mb-3">
-                                        {checkValue()}
-                                        <input
-                                            type="number"
-                                            className="form-control number-custom"
-                                            id="exampleInputEmail1"
-                                            min="1"
-                                            max="20"
-                                            placeholder=""
-                                            value={ethPrice}
-                                            onChange={(event) => {
-                                                setEthPrice(event.target.value);
-                                                setAmount(event.target.value);
-                                            }}
-                                        />
-                                        <span className="input-group-text">
-                                            NFT
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className="col-sm-2 text-center">
-                                    <h1 className="equals">=</h1>
-                                </div>
-
-                                <div className="col-md-5 text-center">
-                                    <div className="input-group mb-3">
-                                        <input
-                                            type="number"
-                                            className="form-control eth-custom"
-                                            id="exampleInputEmail1"
-                                            min="1"
-                                            max="20"
-                                            placeholder=""
-                                            disabled
-                                            value={ethPrice * 0.06}
-                                        />
-                                        <span className="input-group-text">
-                                            ETH
-                                        </span>
-                                    </div>
+                            <h1>
+                                <u>Minting</u>
+                            </h1>
+                        </label>
+                        <div className="row">
+                            <div className="col-md-5 text-center">
+                                <div className="input-group mb-3">
+                                    {checkValue()}
+                                    <input
+                                        type="number"
+                                        className="form-control number-custom"
+                                        id="exampleInputEmail1"
+                                        min="1"
+                                        max="20"
+                                        placeholder=""
+                                        value={ethPrice}
+                                        onChange={(event) => {
+                                            setEthPrice(event.target.value);
+                                            setAmount(event.target.value);
+                                        }}
+                                    />
+                                    <span className="input-group-text">
+                                        NFT
+                                    </span>
                                 </div>
                             </div>
 
-                            <p>
-                                <small>(Maximum of 20)</small> <br />
-                            </p>
-                            <br />
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={buyToken}
-                            >
-                                Confirm
-                            </button>
+                            <div className="col-sm-2 text-center">
+                                <h1 className="equals">=</h1>
+                            </div>
+
+                            <div className="col-md-5 text-center">
+                                <div className="input-group mb-3">
+                                    <input
+                                        type="number"
+                                        className="form-control eth-custom"
+                                        id="exampleInputEmail1"
+                                        min="1"
+                                        max="20"
+                                        placeholder=""
+                                        disabled
+                                        value={ethPrice * 0.06}
+                                    />
+                                    <span className="input-group-text">
+                                        ETH
+                                    </span>
+                                </div>
+                            </div>
                         </div>
+
+                        <p>
+                            <small>(Maximum of 20)</small> <br />
+                        </p>
+                        <br />
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={buyToken}
+                        >
+                            Confirm
+                        </button>
                     </div>
                 </div>
+            </div>
             )}
         </div>
     );
